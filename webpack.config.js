584d452f15d02ca15431a5118b0f98e5bfdaabe9
Mustafa Oladepo <oladepomustafa@yahoo.com>
2018-04-07 18:25:46 +0100

    correct errors in webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,106 @@
-var path = require("path");
+const path = require("path");
 
 module.exports = {
     entry: {
         App: path.join('app', 'assets', 'scripts', 'App.js'),
         Vendor: path.join('app', 'assets', 'scripts', 'Vendor.js')
     },
+
     output: {
         path: path.resolve(__dirname, path.join('app', 'temp', 'scripts')),
         filename: "[name].js"
     },
+
     module: {
-        loaders: [
-            {
-                loader: 'babel-loader',
-                query: {
-                    presets: ['es2015']
-                },
+        rules: [{
                 test: /\.js$/,
-                exclude: /node_modules/
+                exclude: /node_modules/,
+                loader: "babel-loader",
+                options: {
+                    presets: ["es2015"]
+                }
             }
+
         ]
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var path = require("path");
+
+// module.exports = {
+//     entry: {
+//         App: path.join('app', 'assets', 'scripts', 'App.js'),
+//         Vendor: path.join('app', 'assets', 'scripts', 'Vendor.js')
+//     },
+//     output: {
+//         path: path.resolve(__dirname, path.join('app', 'temp', 'scripts')),
+//         filename: "[name].js"
+//     },
+//     module: {
+//         loaders: [
+//             {
+//                 loader: 'babel-loader',
+//                 query: {
+//                     presets: ['es2015']
+//                 },
+//                 test: /\.js$/,
+//                 exclude: /node_modules/
+//             }
+//         ]
+//     }
+// }
